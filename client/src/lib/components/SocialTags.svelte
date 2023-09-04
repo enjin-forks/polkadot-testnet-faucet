bcbda127b4a0be17d6d09c21d877633ec5b957d1
Leonardo Custodio <leonardo@custodio.me>
2023-09-04 11:27:10 -0300

    remove social img

--- client/src/lib/components/SocialTags.svelte
+++ client/src/lib/components/SocialTags.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
   import { MetaTags } from "svelte-meta-tags";
-  import socialImage from "$lib/assets/polkadot.png";
 </script>
 
 <MetaTags
   title="Enjin Canary Faucet"
   description="A fast and reliable Enjin testnet faucet for blockchain developers."
-  openGraph={{
-    images: [{ url: socialImage }],
-  }}
   twitter={{
     title: "Enjin Canary Faucet",
     description: "A fast and reliable Enjin testnet faucet for blockchain developers.",
-    image: socialImage,
     imageAlt: "Enjin Canary Faucet Website",
   }}
 />
