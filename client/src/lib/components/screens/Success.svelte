0ba7c99a930056c38b9827681c3248c8f1efe432
Leonardo Custodio <leonardo@enjin.io>
2023-09-05 11:56:31 -0300

    Update Success.svelte

--- client/src/lib/components/screens/Success.svelte
+++ client/src/lib/components/screens/Success.svelte
@@ -11,11 +11,6 @@
 <div class="message">
   Successfully sent {$testnet.currency}s to your address.
 </div>
-{#if $testnet.explorer}
-  <a href={`${$testnet.explorer}/extrinsic/${hash}`} data-testid="success-button" target="_blank" rel="noreferrer">
-    <button class="submit-btn"> See transaction details</button>
-  </a>
-{/if}
 
 <style lang="postcss">
   .message {
